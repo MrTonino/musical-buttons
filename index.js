e83ee5ed9831f025a38621ebc0989799e5e03845
AC <chidiebere.chuma@gmail.com>
2023-12-23 06:41:56 +0100

    Add jQuery Script Tag

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
-/*
-The code below will achieve thesame result with the one following:
-
-*var clickedButton = document
-  .querySelector("button")
-  .addEventListener("click", handleClick);
-   function handleClick (){
-    alert("I got clicked !")
-   }
-
-
-  */
-/*var clickedButton = document
-  .querySelector("button")
-  .addEventListener("click", function () {
-    alert("I got clicked");
-  }); */
-
+//Detecting button press
 var noOfButtons = document.querySelectorAll(".btn").length;
 
 for (var i = 0; i < noOfButtons; i++) {
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     var btnInnerHTML = this.innerHTML;
-    switch (btnInnerHTML) {
-      case "First":
-        var pat1 = new Audio("music/patobasi1.mp3");
-        pat1.play();
-        break;
-      case "Second":
-        var pat2 = new Audio("music/patobasi2.mp3");
-        pat2.play();
-        break;
-      case "Third":
-        var pat3 = new Audio("music/patobasi3.mp3");
-        pat3.play();
-        break;
-      case "Fourth":
-        var pat4 = new Audio("music/patobasi4.mp3");
-        pat4.play();
-        break;
-      default:
-        alert(btnInnerHTML);
-    }
+    makeSound(btnInnerHTML);
+    buttonAnimation(btnInnerHTML);
   });
 }
 
+//Detecting keyboard press
 document.addEventListener("keydown", function (event) {
-  alert("Key pressed: " + event.key);
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
+
+function makeSound(key) {
+  switch (key) {
+    case "A":
+      var pat1 = new Audio("music/patobasi1.mp3");
+      pat1.play();
+      break;
+    case "B":
+      var pat2 = new Audio("music/patobasi2.mp3");
+      pat2.play();
+      break;
+    case "C":
+      var pat3 = new Audio("music/patobasi3.mp3");
+      pat3.play();
+      break;
+    case "D":
+      var pat4 = new Audio("music/patobasi4.mp3");
+      pat4.play();
+      break;
+    default:
+      alert("Wrong key pressed");
+  }
+}
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+}
